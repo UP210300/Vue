6d7d07c71ed5d3b601e3a901321f8670c757cf7f
UP210300 <up210300@alumnos.upa.edu.mx>
2023-01-04 18:41:21 -0600

    Feat: :sparkles: Vue Router

---
+++ Router/src/views/PokemonView.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import {ref} from "vue";
+import axios from "axios";
+
+const pokemon = ref([]);
+
+
+</script>
+
+<template>
+
+  <h1>Pokemon</h1>
+  <ul>
+    <li v-for="poke in pokemon">
+      {{ poke.name }}
+    </li>
+  </ul>
+
+</template>
+
+<style scoped>
+
+</style>
