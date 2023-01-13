4a349dfe2d5403d8b430e204829f21dd9cc1f427
UP210300 <up210300@alumnos.upa.edu.mx>
2023-01-12 19:05:56 -0600

    Feat: :sparkles: Added API

---
+++ CompositionsAPI/api-flags/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="container">
+    <h1 class="text-center">API Countries</h1>
+    <CardList></CardList>
+  </div>
+
+</template>
+
+<script>
+
+
+import CardList from "@/components/CardList";
+export default {
+  name: 'App',
+  components: {
+    CardList
+
+  }
+}
+</script>
+
+
