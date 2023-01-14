e756542dc2d773336fe4f805fa21a059dd20ab41
UP210300 <up210300@alumnos.upa.edu.mx>
2023-01-13 18:12:19 -0600

    Feat: :sparkles: Added country info

--- CompositionsAPI/api-flags/src/components/CardList.vue
+++ CompositionsAPI/api-flags/src/components/CardList.vue
@@ -1,17 +1,37 @@
 <template>
 <div class="row">
-  <div class="col-12">
-
+  <div
+      class="col-12"
+      v-for="country in countries" :key="country.name"
+  >
+    <Card :country="country"/>
   </div>
 </div>
 </template>
 
 <script>
+
+import {useStore} from 'vuex';
+import {computed, onMounted} from "vue";
+import Card from "@/components/Card";
+
 export default {
-  name: "CardList"
+  components: {Card},
+  setup(){
+
+    const store = useStore()
+
+    const countries = computed(() => {
+      return store.getters.topCountriesPopulation
+    })
+
+    onMounted(() => {
+      store.dispatch('getCountries')
+    })
+
+    return{countries}
+  }
 }
+
 </script>
 
-<style scoped>
-
-</style>
---
+++ CompositionsAPI/api-flags/src/components/Continents.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center">
+    <h3 >Filter by continents</h3>
+    <div class="btn-group mb-4">
+      <button class="btn btn-dark">AM</button>
+      <button class="btn btn-dark">EU</button>
+      <button class="btn btn-dark">AS</button>
+      <button class="btn btn-dark">OC</button>
+      <button class="btn btn-dark">AN</button>
+      <button class="btn btn-dark">ALL</button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Continents"
+}
+</script>
+
+<style scoped>
+
+</style>
